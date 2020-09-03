478530476102b762dc5f2d086c8f07419284bcd2
lukasrieger-dev <rieger.lks@gmail.com>
2020-09-03 16:48:30 +0200

    add comments to config

--- calculator/config.py
+++ calculator/config.py
@@ -1,9 +1,20 @@
 from collections import namedtuple
 
 
-Configuration = namedtuple('Configuration', (
-    'Offset', 'Schnittgeschwindigkeit_mm_s',
-    'Kosten_Schnitt_euro_min', 'Materialgewicht_g_cm3', 'Materialkosten_euro_t',
-    'Gewinnmarge', 'Ausgabespalte', 'Nr_erste_Reihe_Daten', 'Std_Dicke_mm'
-))
+"""
+Import the Configuration NamedTuple to facilitate work with the calculator
+"""
+Configuration = namedtuple('Configuration',
+                           (
+                                'Offset',
+                                'Schnittgeschwindigkeit_mm_s',
+                                'Kosten_Schnitt_euro_min',
+                                'Materialgewicht_g_cm3',
+                                'Materialkosten_euro_t',
+                                'Gewinnmarge',
+                                'Ausgabespalte',
+                                'Nr_erste_Reihe_Daten',
+                                'Std_Dicke_mm',
+                           )
+                          )
 Configuration.__new__.__defaults__ = (0.0,) * len(Configuration._fields)
